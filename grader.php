42400ecd6b9dde051863159559235ab282fc404c
cspenn <cspenn@gmail.com>
2015-12-07 09:32:31 -0500

    Update grader.php
    Added in overall POST variable pull
    Needs, you know, the rest of the entire project

--- grader.php
+++ grader.php
@@ -3,4 +3,32 @@
 /***************************************************
  * This file is where the logic actually goes
  ***************************************************/
+ 
+
+/**
+ * 
+ * 1.	Preg – Replace
+a.	Punctuation
+b.	Special characters
+c.	XSS filter
+d.	Move to LC
+2.	Explode ()
+a.	Spaces
+3.	Release_array ()
+4.	Array Intersect ()
+a.	With Top 100 Static Array
+5.	Cornt State array match
+a.	Total array – array intersect
+6.	Return: 
+a.	$top100;
+b.	$total = array cont
+c.	$words
+7.	Web Page:
+a.	X% overused words
+
+
+**/
+
+$rawrelease = $_POST["textarea"];
+
 ?>
