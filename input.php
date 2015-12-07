51f1f7a7b8c5a5a8b6ae77c85d47bdd2292d0012
cspenn <cspenn@gmail.com>
2015-12-07 09:29:18 -0500

    Update input.php
    added raw form HTML
    This is still uglier than a politician's hairpiece

--- input.php
+++ input.php
@@ -5,3 +5,23 @@ Input form, HTML and PHP goes here, POST target to grader.php
 ****************************************************/
 
 ?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form action="grader.php" method="post" name="form1" id="form1">
+  <p>
+    <label for="textarea">Text Area:</label>
+    <textarea name="textarea" cols="60" rows="20" id="textarea"></textarea>
+  </p>
+  <p>
+    <input type="submit" name="submit" id="submit" value="Here's my press release, bring the pain">
+  </p>
+</form>
+</body>
+</html>
