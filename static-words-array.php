d33840766ca7755d2e249399d4bc353fa4138c0c
SHIFT Communications <marketingteam@shiftcomm.com>
2015-12-08 10:46:59 -0500

    Update static-words-array.php
    added top 100 words

--- static-words-array.php
+++ static-words-array.php
@@ -5,6 +5,6 @@ Top 100 words list
 Static array
 ****************************************************/
 
-$top100 = array_fill('word','banana','word');
+$top100 = array_fill('company', 'more', 'market', 'new', 'million', 'information', 'year', 'business', 'source', 'services', 'global', 'statements', 'related', 'including', 'research', 'industry', 'quarter', 'forward', 'financial', 'looking', 'products', 'based', 'over', 'report', 'technology', 'world', 'management', 'data', 'first', 'results', 'time', 'links', 'companies', 'development', 'growth', 'solutions', 'available', 'share', 'income', 'leading', 'today', 'group', 'sales', 'service', 'most', 'well', 'you', 'photos', 'high', 'years', 'customers', 'product', 'contact', 'president', 'future', 'who', 'media', 'international', 'prnh', 'no', 'up', 'energy', 'percent', 'markets', 'into', 'performance', 'total', 'operating', 'announced', 'securities', 'include', 'investment', 'shares', 'three', 'provides', 'key', 'america', 'release', 'capital', 'day', 'systems', 'provide', 'help', 'across', 'support', 'care', 'cash', 'stock', 'national', 'experience', 'value', 'die', 'operations', 'best', 'please', 'only', 'increase', 'under', 'current', 'program');
 
 ?>
